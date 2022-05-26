a03fbcafe648019bcd821b79463778c6fd7fa0dd
SuperSabre <princewns@gmail.com>
2022-05-26 17:44:38 +0900

    '220526'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AxiosApi from './components/AxiosApi';
+
+function App() {
+  return (
+    <div className="App">
+      <AxiosApi />
+    </div>
+  );
+}
+
+export default App;
